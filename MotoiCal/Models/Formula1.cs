297f8a4061f77b31e28e3de77441573cc802b661
Plus1XP <evldev@protonmail.ch>
2019-12-30 13:33:02 +0000

    Build: Add basic project structure
    Current solution consists of all foreseeable folders and files populated with basic XAML references

---
+++ MotoiCal/Models/Formula1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotoiCal.Models
+{
+    public class Formula1 : IMotorSports
+    {
+    }
+}
