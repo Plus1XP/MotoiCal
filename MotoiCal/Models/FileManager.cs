cbe6dab2de16757f497c2e2daea93bdc1c0dc396
Plus1XP <evldev@protonmail.ch>
2019-12-30 14:41:36 +0000

    Feat: Add CheckFileExists method
    Method returns true or false if parameter (string) filePath exists

--- MotoiCal/Models/FileManager.cs
+++ MotoiCal/Models/FileManager.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace MotoiCal.Models
 {
     public class FileManager
     {
+        public FileManager()
+        {
+
+        }
+
+        public bool CheckFileExists(string filePath)
+        {
+            return File.Exists(filePath) ? true : false;
+        }
     }
 }
