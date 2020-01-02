38bd16375e19fe34ccd675601e2855a3a7c84eb4
Plus1XP <evldev@protonmail.ch>
2020-01-02 13:42:09 +0000

    Feat(DateTime): Add ParseDateTimeToUTC method
    Method returns the parameter (string) dateTime in Universial Time

--- MotoiCal/Models/CalendarManager.cs
+++ MotoiCal/Models/CalendarManager.cs
@@ -8,5 +8,16 @@ namespace MotoiCal.Models
 {
     public class CalendarManager
     {
+        public CalendarManager()
+        {
+
+        }
+
+        public DateTime ParseDateTimeToUTC(string dateTime)
+        {
+            DateTime parsedDateTime;
+            DateTime.TryParse(dateTime, out parsedDateTime);
+            return parsedDateTime.ToUniversalTime();
+        }
     }
 }
