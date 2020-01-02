7f2ec2a5334394db1721a3698683eda82880bee2
Plus1XP <evldev@protonmail.ch>
2020-01-02 20:07:58 +0000

    Feat(iCalendar): Add CreateCalendarTimeZoneEntry method
    Method creates the time zone component of an iCalendar entry

--- MotoiCal/Models/CalendarManager.cs
+++ MotoiCal/Models/CalendarManager.cs
@@ -35,5 +35,25 @@ namespace MotoiCal.Models
             this.calendarEntry.AppendLine("VERSION:2.0");
             this.calendarEntry.AppendLine("PRODID:-//github.com/aleuts//MotoiCal//EN");
         }
+
+        public void CreateCalendarTimeZoneEntry()
+        {
+            // Add the time zone to the calendar item if needed.
+            this.calendarEntry.AppendLine("BEGIN:VTIMEZONE");
+            this.calendarEntry.AppendLine("TZID:GMT Standard Time");
+            this.calendarEntry.AppendLine("BEGIN:STANDARD");
+            this.calendarEntry.AppendLine("DTSTART:16011028T020000");
+            this.calendarEntry.AppendLine("RRULE:FREQ=YEARLY;BYDAY=-1SU;BYMONTH=10");
+            this.calendarEntry.AppendLine("TZOFFSETTO:+0100");
+            this.calendarEntry.AppendLine("TZOFFSETFROM:-0000");
+            this.calendarEntry.AppendLine("END:STANDARD");
+            this.calendarEntry.AppendLine("BEGIN:DAYLIGHT");
+            this.calendarEntry.AppendLine("DTSTART:16010325T010000");
+            this.calendarEntry.AppendLine("RRULE:FREQ=YEARLY;BYDAY=-1SU;BYMONTH=3");
+            this.calendarEntry.AppendLine("TZOFFSETTO:-0000");
+            this.calendarEntry.AppendLine("TZOFFSETFROM:+0100");
+            this.calendarEntry.AppendLine("END:DAYLIGHT");
+            this.calendarEntry.AppendLine("END:VTIMEZONE");
+        }
     }
 }
