d01fa0835c1a2806d669c9f15975e84e48f2ef6e
Plus1XP <evldev@protonmail.ch>
2020-01-06 19:44:09 +0000

    Feat: Add properties to IMotorSports Interface

--- MotoiCal/Models/IMotorSports.cs
+++ MotoiCal/Models/IMotorSports.cs
@@ -8,5 +8,23 @@ namespace MotoiCal.Models
 {
     public interface IMotorSports
     {
+        string FilePath { get; set; }
+        string SportIdentifier { get; set; }
+        string EventTablePath { get; set; }
+        string ClassNamePath { get; set; }
+        string SessionNamePath { get; set; }
+        string RaceNamePath { get; set; }
+        string CircuitNamePath { get; set; }
+        string LocationNamePath { get; set; }
+        string StartDatePath { get; set; }
+        string StartDateAttribute { get; set; }
+        string EndDatePath { get; set; }
+        string EndDateAttribute { get; set; }
+        string GMTOffset { get; set; }
+
+        string[] EventURLs { get; set; }
+        string[] ExcludedClasses { get; set; }
+        string[] ExcludedEvents { get; set; }
+        string[] ExcludedWords { get; set; }
     }
 }
