db8a8b62b975d4d21da00e76eb5f1f0a1caa4a5b
Plus1XP <evldev@protonmail.ch>
2020-01-10 17:23:27 +0000

    Feat(HTMLParser): Add ScrapeEventURL Method
    scrapes the passed in (string URL) for values to add to the iCalendar event entry.

--- MotoiCal/Models/Scraper.cs
+++ MotoiCal/Models/Scraper.cs
@@ -61,5 +61,70 @@ namespace MotoiCal.Models
                 this.ScrapeEventURL(url);
             }
         }
+
+        private void ScrapeEventURL(string url)
+        {
+            this.doc = this.webGet.Load(url);
+
+            string RaceName = this.doc.DocumentNode.SelectSingleNode(this.motorSport.RaceNamePath).InnerText;
+            string LocationName = this.doc.DocumentNode.SelectSingleNode(this.motorSport.LocationNamePath).InnerText;
+            string CircuitName = this.motorSport.CheckForExcludedWords(this.doc.DocumentNode.SelectSingleNode(this.motorSport.CircuitNamePath).InnerText);
+
+            this.resultsOutput.AppendLine($"\n{this.motorSport.SportIdentifier} {RaceName}\n{CircuitName}\n{LocationName}");
+
+            foreach (HtmlNode node in this.doc.DocumentNode.SelectNodes(this.motorSport.EventTablePath))
+            {
+                string ClassName = node.SelectSingleNode(this.motorSport.ClassNamePath).InnerText.Trim();
+
+                // This checks if the node contains any of the substrings in the excluded array.
+                if (this.motorSport.ExcludedClasses.Any(ClassName.Contains))
+                {
+                    continue;
+                }
+
+                string SessionName = this.motorSport.CheckForExcludedWords(node.SelectSingleNode(this.motorSport.SessionNamePath).InnerText.Trim());
+
+                // This checks if the node contains any of the substrings in the excluded array.
+                if (this.motorSport.ExcludedEvents.Any(SessionName.Contains))
+                {
+                    continue;
+                }
+
+                string dtStart = node.SelectSingleNode(this.motorSport.StartDatePath)?.Attributes[this.motorSport.StartDateAttribute].Value;
+
+                // This checks if the node is null or empty, if it is the session is missing on the website and skips over it.
+                if (string.IsNullOrEmpty(dtStart))
+                {
+                    this.resultsOutput.AppendLine($"{ClassName} {SessionName}");
+                    continue;
+                }
+
+                // To handle empty rows ? checks whether or not the returned HtmlNodeCollection is null.
+                string dtEndNullCheck = node.SelectSingleNode(this.motorSport.EndDatePath)?.Attributes[this.motorSport.EndDateAttribute].Value;
+
+                // Handles if the end time result is null (default to start time + 1 HR) or not (parse end time) using a ternary operator.
+                string dtEnd = string.IsNullOrEmpty(dtEndNullCheck) ? DateTime.Parse(dtStart).AddHours(1).ToString() : dtEndNullCheck;
+
+                // Formula1 handles the GMT offset differently (+0000) compared to MotoGP & WSBK (0000-00-00T00:00:00+0000").
+                if (this.motorSport.SportIdentifier.Equals("Formula1"))
+                {
+                    string dtOffset = node.SelectSingleNode(this.motorSport.StartDatePath).Attributes[this.motorSport.GMTOffset].Value;
+                    dtStart += dtOffset;
+                    dtEnd += dtOffset;
+                }
+
+                // Use UTC time for iCal event.
+                DateTime StartTimeUTC = this.iCalendar.ParseDateTimeToUTC(dtStart);
+                DateTime EndTimeUTC = this.iCalendar.ParseDateTimeToUTC(dtEnd);
+
+                // Use local time for console results.
+                DateTime StartTimeLocal = this.iCalendar.ParseDateTimeToLocal(dtStart);
+                DateTime EndTimeLocal = this.iCalendar.ParseDateTimeToLocal(dtEnd);
+
+                this.resultsOutput.AppendLine($"{ClassName} {SessionName} : {StartTimeLocal} - {EndTimeLocal}");
+
+                this.iCalendar.CreateCalendarEventEntry(StartTimeUTC, EndTimeUTC, $"{ClassName} {RaceName} {SessionName}", LocationName, CircuitName);
+            }
+        }
     }
 }
