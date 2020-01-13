7d54610fa29099bd60dd80879d20a9b348896b9c
Plus1XP <evldev@protonmail.ch>
2020-01-13 17:40:06 +0000

    Feat: Add Championship Enum Types
    To select what championship series should be searched.

---
+++ MotoiCal/Models/Championship.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotoiCal.Models
+{
+    public enum Championship
+    {
+        None,
+        Formula1,
+        MotoGP,
+        WSBK
+    }
+}
