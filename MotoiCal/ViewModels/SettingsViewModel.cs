feb655aee92a04a17defd4467a00f5d4818121c5
Plus1XP <evldev@protonmail.ch>
2020-08-29 11:47:33 +0100

    Feat(UI): Add ViewModels & Views for Dock Panel Headings User Controls

---
+++ MotoiCal/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MotoiCal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MotoiCal.ViewModels
+{
+    class SettingsViewModel
+    {
+        public string Name { get; } = "WorldSBK Working!";
+    }
+}
