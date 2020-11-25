d55b0ba7ece8b73362c54d363f6be51116d879ec
Plus1XP <evldev@protonmail.ch>
2020-11-25 01:07:15 +0000

    Feat(EasterEgg): Add DateToRGBConverter to change font colour based off Easter Egg's Trigger Date

---
+++ MotoiCal/ViewModels/DateToRGBConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+using MotoiCal.ViewModels;
+
+namespace MotoiCal.ViewModels
+{
+    class DateToRGBConverter : IValueConverter
+    {
+        //private readonly Brush NormalAccent = new SolidColorBrush(Color.FromRgb(0, 122, 204)); // Blue
+        private readonly Brush NormalAccent = new SolidColorBrush(Colors.Gray);
+        private readonly Brush SpecialAccent = new SolidColorBrush(Color.FromRgb(56, 176, 157)); // Green
+
+        private readonly string SpecialVersion = "\"Hammertime\"";
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Settings.AboutContentViewModel about = new Settings.AboutContentViewModel();
+
+            if (about.IsEasterEggActive() && about.VersionName == this.SpecialVersion)
+            {
+                return this.SpecialAccent;
+            }
+            else
+            {
+                return this.NormalAccent;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
