85795c6b2861213af829f45daa5bf74273790fee
Plus1XP <evldev@protonmail.ch>
2021-01-12 23:32:54 +0000

    Refactor(Scraper): Completely Refactor new scraper class

--- MotoiCal/Services/MotorSportScraper.cs
+++ MotoiCal/Services/MotorSportScraper.cs
@@ -4,6 +4,7 @@ using MotoiCal.Enums;
 using MotoiCal.Interfaces;
 using MotoiCal.Models;
 using MotoiCal.Models.FileManagement;
+using MotoiCal.Utilities.Helpers;
 
 using System;
 using System.Collections.Generic;
@@ -11,7 +12,6 @@ using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
-using System.Threading;
 
 namespace MotoiCal.Services
 {
@@ -20,7 +20,6 @@ namespace MotoiCal.Services
         private HtmlDocument doc;
         private readonly HtmlWeb webGet;
         private readonly CalendarManager iCalendar;
-        private ObservableCollection<IRaceTimeTable> timeTable;  //REFACTOR THIS
 
         public MotorSportScraper()
         {
@@ -29,42 +28,29 @@ namespace MotoiCal.Services
             this.iCalendar = new CalendarManager();
         }
 
-        public void ScrapeSeries(MotorSport motorSport)
+        public ObservableCollection<IRaceTimeTable> ScrapeSeries(MotorSport motorSport)
         {
-            this.timeTable = new ObservableCollection<IRaceTimeTable>();
-            PopulateRaceURL(motorSport);
+            Stopwatch stopWatch1 = Stopwatch.StartNew();
+            ObservableCollection<IRaceTimeTable> timeTable = new ObservableCollection<IRaceTimeTable>();
+            this.PopulateRaceURL(motorSport);
             foreach (string url in motorSport.EventUrlList)
             {
+                Stopwatch stopWatch2 = Stopwatch.StartNew();
                 bool isEventSkipped = false;
-                SetHTMLParams(url);
+                this.GetHTMLDoc(url);
                 this.GetRaceHeaders(motorSport);
                 this.GetEventTimeTable(motorSport, isEventSkipped);
-                AddTimeTableToList(motorSport, timeTable, isEventSkipped);
+                this.AddTimeTableToList(motorSport, timeTable, isEventSkipped);
+                stopWatch2.Stop();
+                Debug.WriteLine($"Page scrape search time: {stopWatch2.Elapsed.Seconds}.{stopWatch2.Elapsed.Milliseconds / 10}");
             }
+            stopWatch1.Stop();
+            Debug.WriteLine($"Total Operation Time: {stopWatch1.Elapsed.Seconds}.{stopWatch1.Elapsed.Milliseconds / 10}");
+
+            return timeTable;
         }
 
-        private void PopulateRaceURL(MotorSport motorSport)
-        {
-            if (motorSport.EventUrlList?.Any() != true)
-            {
-                this.doc = this.webGet.Load(motorSport.Url);
-                motorSport.EventUrlList = new List<string>();
-
-                foreach (HtmlNode node in this.doc.DocumentNode.SelectNodes(motorSport.UrlPath))
-                {
-                    string scrapedUrl = node.Attributes[motorSport.UrlAttribute]?.Value;
-                    string url = string.IsNullOrEmpty(scrapedUrl) ? url = "URL not found" : url = $"{motorSport.UrlPartial}{scrapedUrl}";
-                    if (motorSport.ExcludedUrls.Any(url.Contains))
-                    {
-                        continue;
-                    }
-                    motorSport.EventUrlList.Add(url);
-                    Debug.WriteLine(url);
-                }
-            }
-        }
-
-        private void SetHTMLParams(string url)
+        private void GetHTMLDoc(string url)
         {
             // The HTMLWeb parameters set the encoding to the URL, otherwise special characters wont display correctly.
             this.webGet.AutoDetectEncoding = false;
@@ -73,33 +59,70 @@ namespace MotoiCal.Services
             this.doc = this.webGet.Load(url);
         }
 
-        private void GetRaceHeaders(MotorSport motorSport)
+        private void PopulateRaceURL<T>(T motorSport) where T : IDocNodePath, IDocExclusionList
         {
-            motorSport.GrandPrix = GetGrandPrix(motorSport);
-            motorSport.Sponser = GetSponser(motorSport);
-            motorSport.Location = GetLocation(motorSport, motorSport.Sponser);
+            // Checks list, Same as if list == null or motorSport.Count == 0
+            if (motorSport.EventUrlList?.Any() != true)
+            {
+                motorSport.EventUrlList = new List<string>();
+                this.GetHTMLDoc(motorSport.Url);
+                this.GetUrlList(motorSport);
+            }           
         }
 
-        private void GetEventTimeTable(MotorSport motorSport, bool isEventSkipped)
+        private void GetUrlList<T>(T motorSport) where T : IDocNodePath, IDocExclusionList
         {
+            Stopwatch stopWatch3 = Stopwatch.StartNew();
+            foreach (HtmlNode node in this.doc.DocumentNode.SelectNodes(motorSport.UrlPath))
+            {
+                string scrapedUrl = node.Attributes[motorSport.UrlAttribute]?.Value;
+                string url = string.IsNullOrEmpty(scrapedUrl) ? "URL not found" : $"{motorSport.UrlPartial}{scrapedUrl}";
+                Debug.WriteLine($"{url} : {scrapedUrl}");
+                if (this.CheckExcludedURL(motorSport, url) || url.Equals("URL not found"))
+                {
+                    Debug.WriteLine($"Skipped {url}");
+                    continue;
+                }
+                motorSport.EventUrlList.Add(url);
+                Debug.WriteLine($"Added: {url}");
+            }
+            stopWatch3.Stop();
+            Debug.WriteLine($"URL Collection search time: {stopWatch3.Elapsed.Seconds}.{stopWatch3.Elapsed.Milliseconds / 10}");
+        }
+
+        private void GetRaceHeaders<T>(T motorSport) where T : IRaceTimeTable, IDocNodePath
+        {
+            motorSport.GrandPrix = this.GetGrandPrix(motorSport);
+            motorSport.Sponser = this.GetSponser(motorSport);
+            motorSport.Location = this.GetLocation(motorSport, motorSport.Sponser);
+        }
+
+        // Some Nodes return null if there is a problem with the paths or the data is missing.
+        // "?" checks and allows the returned HtmlNodeCollection to be null, "??" returns a string if the node is null.
+        private void GetEventTimeTable<T>(T motorSport, bool isEventSkipped) where T : IRaceTimeTable, IDocNodePath, IDocExclusionList
+        {
+            //Debug.Assert(this.doc.DocumentNode.SelectNodes(motorSport.EventTablePath) != null);
+            //Debug.Assert(!GrandPrix.Contains("Spain"));
+            //Debug.WriteIf(motorSport.Url.Contains("https://www.worldsbk.com/en/event/ESP3/2020"), $"{GrandPrix}");
+
+            // MotoGP are updated the schedule, eventsTable loads 404.
             if (this.doc.DocumentNode.SelectNodes(motorSport.EventTablePath) != null)
             {
                 foreach (HtmlNode node in this.doc.DocumentNode.SelectNodes(motorSport.EventTablePath))
                 {
-                    motorSport.Series = GetSeries(motorSport, node, isEventSkipped);
-                    motorSport.Session = GetSession(motorSport, node, isEventSkipped);
-                    Tuple<string, string> dateTime = GetDateTime(motorSport, node, isEventSkipped);
-                    Tuple<DateTime, DateTime> dateTimeUTC = ParseDateTimeUTC(dateTime);
-                    motorSport.StartUTC = dateTimeUTC.Item1;
-                    motorSport.EndUTC = dateTimeUTC.Item2;
-                    Tuple<DateTime, DateTime> dateTimeLocal = ParseDateTimeLocal(dateTime);
-                    motorSport.Start = dateTimeLocal.Item1;
-                    motorSport.End = dateTimeLocal.Item2;
+                    motorSport.Series = this.GetSeries(motorSport, node, isEventSkipped);
+                    motorSport.Session = this.GetSession(motorSport, node, isEventSkipped);
+                    Tuple<string, string> dateTime = this.GetDateTime(motorSport, node, isEventSkipped);
+                    motorSport.Start = this.ParseDateTimeLocal(dateTime).Item1;
+                    motorSport.End = this.ParseDateTimeLocal(dateTime).Item2;
+                    motorSport.StartUTC = this.ParseDateTimeUTC(dateTime).Item1;
+                    motorSport.EndUTC = this.ParseDateTimeUTC(dateTime).Item2;
+
                 }
             }
             else
             {
-                Debug.WriteLine($"Event Missing: url??");
+                Debug.WriteLine($"Event Missing: {motorSport.GrandPrix}");
             }
         }
 
@@ -117,6 +140,7 @@ namespace MotoiCal.Services
         {
             // Location is empty in WSBK Catalunya's URL.
             string Location = this.doc.DocumentNode.SelectSingleNode(motorSport.LocationNamePath)?.InnerText ?? "No Data";
+
             // Adds location to empty WSBK Catalunya.
             return this.WSBKCatalunyaLoctionFix(Sponser, Location);
         }
@@ -134,33 +158,29 @@ namespace MotoiCal.Services
             }
         }
 
-        private string GetSeries(MotorSport motorSport, HtmlNode node, bool isEventSkipped)
+        private string GetSeries<T>(T motorSport, HtmlNode node, bool isEventSkipped) where T : IDocNodePath, IDocExclusionList
         {
             string Series = node.SelectSingleNode(motorSport.SeriesNamePath).InnerText.Trim();
 
             // This checks if the node contains any of the substrings in the excluded array.
-            if (motorSport.ExcludedClasses.Any(Series.Contains))
-            {
-                isEventSkipped = true;
-            }
+            isEventSkipped = this.ChecExcludedClasses(motorSport, Series);
 
             return Series;
         }
 
-        private string GetSession(MotorSport motorSport, HtmlNode node, bool isEventSkipped)
+        private string GetSession<T>(T motorSport, HtmlNode node, bool isEventSkipped) where T : IDocNodePath, IDocExclusionList
         {
-            string Session = motorSport.CheckForExcludedWords(node.SelectSingleNode(motorSport.SessionNamePath).InnerText.Trim());
+            //string Session = motorSport.CheckForExcludedWords(node.SelectSingleNode(motorSport.SessionNamePath).InnerText.Trim());
+            string Session = string.Empty;
+            Session.CheckForExcludedWords(node.SelectSingleNode(motorSport.SessionNamePath).InnerText.Trim(), motorSport.ExcludedWords);
 
             // This checks if the node contains any of the substrings in the excluded array.
-            if (motorSport.ExcludedEvents.Any(Session.Contains))
-            {
-                isEventSkipped = true;
-            }
+            isEventSkipped = this.CheckExcludedEvents(motorSport, Session);
 
             return Session;
         }
 
-        private Tuple<string, string> GetDateTime(MotorSport motorSport, HtmlNode node, bool isEventSkipped)
+        private Tuple<string, string> GetDateTime<T>(T motorSport, HtmlNode node, bool isEventSkipped) where T : IRaceTimeTable, IDocNodePath
         {
             string dtStart = node.SelectSingleNode(motorSport.StartDatePath)?.Attributes[motorSport.StartDateAttribute].Value;
 
@@ -187,22 +207,22 @@ namespace MotoiCal.Services
             return Tuple.Create(dtStart, dtEnd);
         }
 
-        private Tuple<DateTime, DateTime> ParseDateTimeUTC(Tuple<string, string> dateTime)
-        {
-            // Use UTC time for iCal event.
-            DateTime StartUTC = this.iCalendar.ParseDateTimeToUTC(dateTime.Item1);
-            DateTime EndUTC = this.iCalendar.ParseDateTimeToUTC(dateTime.Item2);
-
-            return Tuple.Create(StartUTC, EndUTC);
-        }
-
+        // Use local time for console results.
         private Tuple<DateTime, DateTime> ParseDateTimeLocal(Tuple<string, string> dateTime)
         {
-            // Use local time for console results.
             DateTime Start = this.iCalendar.ParseDateTimeToLocal(dateTime.Item1);
             DateTime End = this.iCalendar.ParseDateTimeToLocal(dateTime.Item2);
 
             return Tuple.Create(Start, End);
+        }
+
+        // Use UTC time for iCal event.
+        private Tuple<DateTime, DateTime> ParseDateTimeUTC(Tuple<string, string> dateTime)
+        {
+            DateTime StartUTC = this.iCalendar.ParseDateTimeToUTC(dateTime.Item1);
+            DateTime EndUTC = this.iCalendar.ParseDateTimeToUTC(dateTime.Item2);
+
+            return Tuple.Create(StartUTC, EndUTC);
         }
 
         private void AddTimeTableToList(IRaceTimeTable motorSport, ObservableCollection<IRaceTimeTable> timeTable, bool isEventSkipped)
@@ -211,165 +231,21 @@ namespace MotoiCal.Services
             {
                 timeTable.Add(motorSport);
             }
-            //switch (motorSport.SportIdentifier)
-            //{
-            //    case MotorSportID.None:
-            //        break;
-            //    case MotorSportID.Formula1:
-            //        timeTable.Add(motorSport);
-            //        break;
-            //    case MotorSportID.MotoGP:
-            //        timeTable.Add(motorSport);
-            //        break;
-            //    case MotorSportID.WorldSBK:
-            //        timeTable.Add(motorSport);
-            //        break;
-            //    default:
-            //        break;
-            //}
         }
 
-        // Code below not is use ??
-
-        /*
-                public int EventsFound()
-                {
-                    return this.raceData.Count;
-                }
-
-                public int RacesFound(IDocNodePath motorSport) //REFACTOR THIS
-                {
-                    return motorSport.EventUrlList.Count;
-                }
-
-                public bool IsEasterEggActive(string easterEggDate)
-                {
-                    return DateTime.Parse(easterEggDate) == DateTime.Now.Date ? true : false;
-                }
-        */
-
-        // currentSponser is initially set to null, then each loop is given the current Sponser value.
-        // This allows the stringbuilder to check if it needs to update header.
-        // * This was changed from checking again the currentGrandPrix in the COVID update due to multiple races at the same GrandPrix.
-      
-
-
-
-        public string ScrapeEventsToiCalendar(MotorSport motorSport) //REFACTOR THIS
+        private bool CheckExcludedURL(IDocExclusionList motorSport, string url)
         {
-            Stopwatch stopWatch = Stopwatch.StartNew();
-            this.timeTable = new ObservableCollection<IRaceTimeTable>();
-            // Checks list, Same as if list == null or motorSport.Count == 0
-            if (motorSport.EventUrlList?.Any() != true)
-            {
-                this.AddMotoSportEventsToList(motorSport);
-            }
-            this.ProcessMotorSportEvents(motorSport);
-            stopWatch.Stop();
-            Debug.WriteLine($"Total Operation Time: {stopWatch.Elapsed.Seconds}.{stopWatch.Elapsed.Milliseconds / 10}");
-            return "Removed method";
+            return motorSport.ExcludedUrls.Any(url.Contains);
         }
 
-        private void AddMotoSportEventsToList(MotorSport motorSport) //REFACTOR THIS
+        private bool ChecExcludedClasses(IDocExclusionList motorSport, string series)
         {
-            Stopwatch stopWatch = Stopwatch.StartNew();
-            
-            stopWatch.Stop();
-            Debug.WriteLine($"URL Collection search time: {stopWatch.Elapsed.Seconds}.{stopWatch.Elapsed.Milliseconds / 10}");
+            return motorSport.ExcludedClasses.Any(series.Contains);
         }
 
-        private void ProcessMotorSportEvents(MotorSport motorSport) //REFACTOR THIS
+        private bool CheckExcludedEvents(IDocExclusionList motorSport, string events)
         {
-            Stopwatch stopWatch = Stopwatch.StartNew();
-            //Parallel.ForEach(motorSport.EventUrlList, async url =>
-            //{
-            //    await this.FindMotorSportSessions(motorSport, url);
-            //    Debug.WriteLine($"Thread No. {Thread.CurrentThread.ManagedThreadId}");
-            //    //Thread.Sleep(50);
-            //});
-            
-            stopWatch.Stop();
-            Debug.WriteLine($"Total search time: {stopWatch.Elapsed.Seconds}.{stopWatch.Elapsed.Milliseconds / 10}");
-        }
-
-        // Some Nodes return null if there is a problem with the paths or the data is missing.
-        // "?" checks and allows the returned HtmlNodeCollection to be null, "??" returns a string if the node is null.
-        private void FindMotorSportSessions(MotorSport motorSport, string url) //REFACTOR THIS
-        {
-            Stopwatch stopWatch = Stopwatch.StartNew();
-
-
-            stopWatch.Stop();
-            Debug.WriteLine($"Page scrape search time: {stopWatch.Elapsed.Seconds}.{stopWatch.Elapsed.Milliseconds / 10}");
-
-
-
-            //Debug.Assert(this.doc.DocumentNode.SelectNodes(motorSport.EventTablePath) != null);
-            //Debug.Assert(!GrandPrix.Contains("Spain"));
-            //Debug.WriteIf(motorSport.Url.Contains("https://www.worldsbk.com/en/event/ESP3/2020"), $"{GrandPrix}");
-
-            // MotoGP are updated the schedule, eventsTable loads 404.
-
-        }
-
-        private void RaceTimeTableModel(
-            IRaceTimeTable motorSport, //REFACTOR THIS
-            string grandPrix,
-            string location,
-            string sponser,
-            string series,
-            string session,
-            DateTime start,
-            DateTime end,
-            DateTime startUTC,
-            DateTime endUTC
-            )
-        {
-            switch (motorSport.SportIdentifier)
-            {
-                case MotorSportID.Formula1:
-                    this.timeTable.Add(new Formula1
-                    {
-                        GrandPrix = grandPrix,
-                        Location = location,
-                        Sponser = sponser,
-                        Series = series,
-                        Session = session,
-                        Start = start,
-                        End = end,
-                        StartUTC = startUTC,
-                        EndUTC = endUTC
-                    });
-                    break;
-                case MotorSportID.MotoGP:
-                    this.timeTable.Add(new MotoGP
-                    {
-                        GrandPrix = grandPrix,
-                        Location = location,
-                        Sponser = sponser,
-                        Series = series,
-                        Session = session,
-                        Start = start,
-                        End = end,
-                        StartUTC = startUTC,
-                        EndUTC = endUTC
-                    });
-                    break;
-                case MotorSportID.WorldSBK:
-                    this.timeTable.Add(new WorldSBK
-                    {
-                        GrandPrix = grandPrix,
-                        Location = location,
-                        Sponser = sponser,
-                        Series = series,
-                        Session = session,
-                        Start = start,
-                        End = end,
-                        StartUTC = startUTC,
-                        EndUTC = endUTC
-                    });
-                    break;
-            }
+            return motorSport.ExcludedEvents.Any(events.Contains);
         }
     }
 }
