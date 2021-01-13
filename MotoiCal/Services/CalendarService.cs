56170241b691438499ce565c18dbde73df961910
Plus1XP <evldev@protonmail.ch>
2021-01-13 16:57:51 +0000

    Feat(Service): Add CalendarService Class

---
+++ MotoiCal/Services/CalendarService.cs
@@ -0,0 +1,12 @@
+﻿using MotoiCal.Interfaces;
+using MotoiCal.Models;
+using MotoiCal.Models.FileManagement;
+
+using System.Collections.ObjectModel;
+
+namespace MotoiCal.Services
+{
+    class CalendarService
+    {
+    }
+}
