543eb05858aba964f161c26f27678f0eb60192be
Plus1XP <evldev@protonmail.ch>
2021-01-13 16:58:56 +0000

    Feat(Service): Add GenerateiCalendar method

--- MotoiCal/Services/CalendarService.cs
+++ MotoiCal/Services/CalendarService.cs
@@ -8,5 +8,26 @@ namespace MotoiCal.Services
 {
     class CalendarService
     {
+        private readonly CalendarManager iCalendar;
+
+        public CalendarService()
+        {
+            this.iCalendar = new CalendarManager();
+        }
+
+        public string GenerateiCalendar(ICalendarEvent motorSport, ObservableCollection<IRaceTimeTable> timeTable)
+        {
+            // Checks if url list has a value, if not then it is assumed the dates have not been pulled.
+            //if (motorSport.EventUrlList?.Any() != true)
+            if (string.IsNullOrEmpty(motorSport.IcalendarLocation))
+            {
+                return "Can not generate ICS file without first showing dates";
+            }
+            else
+            {
+                this.ProcessiCalendarResults(timeTable, motorSport.IsEventReminderActive, motorSport.EventReminderMins);
+                return this.iCalendar.CreateICSFile(motorSport.FilePath);
+            }
+        }
     }
 }
