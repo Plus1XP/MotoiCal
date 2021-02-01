e7ff38a54de281fce47fe69d1f4bbe124586cd8c
Plus1XP <evldev@protonmail.ch>
2021-02-01 16:33:12 +0000

    Feat(Converters): Add InverseBooleanToVisibility Converter

---
+++ MotoiCal/Utilities/Converters/InverseBooleanToVisibilityConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace MotoiCal.Utilities.Converters
+{
+    class InverseBooleanToVisibilityConveter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? Visibility.Hidden : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((Visibility)value == Visibility.Visible)
+            {
+                return false;
+            }
+
+            if ((Visibility)value == Visibility.Hidden)
+            {
+                return true;
+            }
+
+            throw new Exception(string.Format("Cannot convert back, unknown value {0}", value));
+        }
+    }
+}
