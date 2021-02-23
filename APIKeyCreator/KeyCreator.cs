4a4bd8030395ac1f26b20f983db680510f5920b2
Plus1XP <evldev@protonmail.ch>
2021-02-23 13:16:47 +0000

    Feat(APIKey): Add Project to create me ApiKey

---
+++ APIKeyCreator/KeyCreator.cs
@@ -0,0 +1,35 @@
+﻿using MotoiCal.Models.FileManagement;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIKeyCreator
+{
+    class KeyCreator
+    {
+        private EncryptionManager encryptionManager;
+
+        // This will get the current PROJECT directory
+        private string projectDirectory = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+
+        private string plainTextFilePath;
+        private string encryptedFilePath;
+        public KeyCreator()
+        {
+            encryptionManager = new EncryptionManager();
+            plainTextFilePath = $"{projectDirectory}\\motoical\\apikey.txt";
+            encryptedFilePath = $"{projectDirectory}\\motoical\\apikey";
+    }
+
+        public string CreateApiKeyFile()
+        {
+            string apikey = encryptionManager.ReadFromFile(plainTextFilePath);
+            encryptionManager.EncryptFile(encryptionManager.Password, apikey, encryptedFilePath);
+            return "Key File Created";
+        }
+    }
+}
