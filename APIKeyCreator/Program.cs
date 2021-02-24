ace57ecc62d441d2d0c484b9a10a6de952202c08
Plus1XP <evldev@protonmail.ch>
2021-02-24 14:18:52 +0000

    Feat(APIKey): Add Main Method for APIKey Creator

---
+++ APIKeyCreator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIKeyCreator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KeyCreator keyCreator = new KeyCreator();
+            string result = keyCreator.CreateApiKeyFile();
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
