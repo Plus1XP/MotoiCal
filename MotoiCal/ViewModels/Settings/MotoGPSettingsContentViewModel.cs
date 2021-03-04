493f93a6265b1f139935298b8bf73813d4e7a307
Plus1XP <evldev@protonmail.ch>
2021-03-04 17:27:24 +0000

    Feat(ViewModel): Add Moto2 & 3 visibility & saved bool to MotoGP settings content view

--- MotoiCal/ViewModels/Settings/MotoGPSettingsContentViewModel.cs
+++ MotoiCal/ViewModels/Settings/MotoGPSettingsContentViewModel.cs
@@ -6,6 +6,8 @@ namespace MotoiCal.ViewModels.Settings
     class MotoGPSettingsContentViewModel : SettingsContentViewModel
     {
         private bool isWarmUpSaved;
+        private bool isMoto2Saved;
+        private bool isMoto3Saved;
         private bool isBehindTheScenesSaved;
         private bool isAfterTheFlagSaved;
 
@@ -14,10 +16,14 @@ namespace MotoiCal.ViewModels.Settings
             this.IsQualifyingVisible = true;
             this.IsSuperpoleVisible = false;
             this.IsWarmupVisible = true;
+            this.IsMoto2Visible = true;
+            this.IsMoto3Visible = true;
             this.IsBehindTheScenesVisible = true;
             this.IsAfterTheFlagVisible = true;
 
             this.IsWarmUpSaved = this.SettingsData.GetToggleSwitchValue("Warmup");
+            this.IsMoto2Saved = this.SettingsData.GetToggleSwitchValue("Moto2");
+            this.IsMoto3Saved = this.SettingsData.GetToggleSwitchValue("Moto3");
             this.IsBehindTheScenesSaved = this.SettingsData.GetToggleSwitchValue("BehindTheScenes");
             this.IsAfterTheFlagSaved = this.SettingsData.GetToggleSwitchValue("AfterTheFlag");
         }
@@ -25,6 +31,8 @@ namespace MotoiCal.ViewModels.Settings
         public override bool IsQualifyingVisible { get; }
         public override bool IsSuperpoleVisible { get; }
         public override bool IsWarmupVisible { get; }
+        public override bool IsMoto2Visible { get; }
+        public override bool IsMoto3Visible { get; }
         public override bool IsBehindTheScenesVisible { get; }
         public override bool IsAfterTheFlagVisible { get; }
 
@@ -40,6 +48,36 @@ namespace MotoiCal.ViewModels.Settings
                 this.UpdateIMotorSportEvenList(value, "Warm Up");
                 this.SettingsData.SetToggleSwitchValue("Warmup", value);
                 this.OnPropertyChanged("IsWarmUpSaved");
+            }
+        }
+
+        public bool IsMoto2Saved
+        {
+            get
+            {
+                return this.isMoto2Saved;
+            }
+            set
+            {
+                this.isMoto2Saved = value;
+                this.UpdateIMotorSportSeriesList(value, "Moto2");
+                this.SettingsData.SetToggleSwitchValue("Moto2", value);
+                this.OnPropertyChanged("IsMoto2Saved");
+            }
+        }
+
+        public bool IsMoto3Saved
+        {
+            get
+            {
+                return this.isMoto3Saved;
+            }
+            set
+            {
+                this.isMoto3Saved = value;
+                this.UpdateIMotorSportSeriesList(value, "Moto3");
+                this.SettingsData.SetToggleSwitchValue("Moto3", value);
+                this.OnPropertyChanged("IsMoto2Saved");
             }
         }
 
